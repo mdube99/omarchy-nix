6287f666c74202b3c0f70b97af00f695d3fb5d1f
mdube99 <mjdube99@gmail.com>
2025-07-04 11:06:40 -0500

    removed vscode

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -19,7 +19,6 @@ in {
     (import ./git.nix)
     (import ./mako.nix)
     (import ./starship.nix)
-    (import ./vscode.nix)
     (import ./waybar.nix)
     (import ./wofi.nix)
     (import ./zoxide.nix)
